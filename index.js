b1cdb4420bfbb0dc17bba5768c425e174be82f8f
Charlene Johnson <charcodes24@gmail.com>
2021-04-30 18:20:15 -0700

    Completed assignment.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+// function superBowlWin(record) {
+//     if (record.result === 'W') {
+//         return record.year;
+//     };
+// };
 
+// function superBowlWin(seasons) {
+//     const wins = seasons.find(function (element) {
+//         return element.result === 'W';
+//     });
+// }
+
+// function superBowlWin(seasons) {
+//     const champs = seasons.find(function(element) {
+//         return element.result === 'W';
+//     });
+//     return champs.year;
+// };
+
+function superbowlWin(seasons) {
+    const champs = seasons.find(function(element) {
+        return element.result === 'W';
+    });
+    // return champs.year;
+    return champs ? champs.year : champs;
+}
